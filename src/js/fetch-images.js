c08b7e9a23c683b0b4ddf8e42a1d6d0660429ed6
Oksana Tonkopriad <tonkopryad84@gmail.com>
2022-10-02 17:52:57 +0300

    save

--- src/js/fetch-images.js
+++ src/js/fetch-images.js
@@ -2,12 +2,4 @@ import axios from "axios";
 
 const axios = require('axios');
 
-export default async function fetchImages(value, page) {
-    const url = 'https://pixabay.com/api/';
-    const key = '14595272-b438f74d87c5b1f828750b62e';
-    const filter = `?key=${key}&q=${value}&image_type=photo&orientation=horizontal&safesearch=true&per-page=40&page=${page}`;
-
-    return await axios.get(`${url}${filter}`)
-        .then(response => response.data);
-
-}
+export default async function fetchImages()
