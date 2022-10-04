b8ce6b7c432e5ccb2ca954f7e08f46a60cd11b2d
Oksana Tonkopriad <tonkopryad84@gmail.com>
2022-10-05 01:13:46 +0300

    save

--- src/index.js
+++ src/index.js
@@ -1,9 +1,26 @@
 import getRefs from "./js/getRefs";
-import fetchImages from './js/fetch-images';
-import getRefs from "./js/getRefs";
+import FetchImages from './js/fetch-images';
 import { Notify } from 'notiflix';
 import cardTemplate from './templates/card-template.hbs';
 
 const refs = getRefs();
 
 refs.searchForm.addEventListener(`submit`, onSearchForm);
+refs.moreBtn.addEventListener(`click`, onLoadMore);
+
+const fetchImages = new FetchImages();
+
+function onSearchForm(e) {
+    e.preventDefault();
+
+    fetchImages.query = e.currentTarget.elements.searchQuery.value.trim();
+    fetchImages.resetPage();
+    fetchImages.fetchArticles().then(articles => console.log(articles));
+    
+    
+}
+
+function onLoadMore() {
+  fetchImages.fetchArticles().then(articles => console.log(articles));
+}
+
--- src/js/fetch-images.js
+++ src/js/fetch-images.js
@@ -2,4 +2,44 @@ import axios from "axios";
 
 const axios = require('axios');
 
-export default async function fetchImages()
+export default class FetchImages {
+    constructor() {
+        this.searchQuery = '';
+        this.page = 1;
+        this.per_page = 40;
+    }
+
+    fetchArticles() {
+    const options = {
+    headers: {
+        Authorization: '14595272-b438f74d87c5b1f828750b62e',
+    },
+};
+    const url =
+        `https://pixabay.com/api/everything?q=${this.searchQuery}&image_type=photo&orientation=horizontal&safesearch=true&per_page=40&page=${this.page}`;
+
+return fetch(url, options)
+    .then(r => r.json())
+    .then(data => {
+        this.nextPage();
+        return data.articles;
+    });
+    }
+
+    nextPage() {
+        this.page += 1;
+    }
+
+    resetPage() {
+        this.page = 1;
+    }
+
+    get query() {
+        return this.searchQuery;
+    }
+
+    set query(newQuery) {
+        this.searchQuery = newQuery;
+    }
+}
+
