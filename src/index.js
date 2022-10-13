ad3ddf0bb32ee43f117bd95342006b5a33b5c08a
Oksana Tonkopriad <tonkopryad84@gmail.com>
2022-10-14 01:03:05 +0300

    save

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import getRefs from "./js/getRefs";
 import FetchImages from './js/fetch-images';
 import { Notify } from 'notiflix';
-import cardTemplate from './templates/card-template.hbs';
+// import cardTemplate from './templates/card-template.hbs';
 
 const refs = getRefs();
 
@@ -11,25 +11,59 @@ refs.moreBtn.addEventListener(`click`, onLoadMore);
 const fetchImages = new FetchImages();
 
 function onSearchForm(e) {
-    e.preventDefault();
-
+  e.preventDefault();
+  
   clearGallery();
-    fetchImages.query = e.currentTarget.elements.searchQuery.value.trim();
-    fetchImages.resetPage();
-    fetchImages.fetchArticles().then(appendHitsMarkup);
-    
-    
+  fetchImages.query = e.currentTarget.elements.searchQuery.value.trim();
+  fetchImages.resetPage();
+  if (fetchImages.query === '') {
+    refs.moreBtn.disable();
+    return Notify.failure('Sorry, there are no images matching your search query. Please try again.');
+  }
+
+  fetchImages.fetchArticles().then(appendHitsMarkup);
+  
+  // const response = await fetchImages(query, page)
+  e.target.reset();
 }
 
 function onLoadMore() {
   fetchImages.fetchArticles().then(appendHitsMarkup);
 }
 
-function appendHitsMarkup(hits) {
-  const markup = hits.map(item => cardTemplate(item)).join('');
-  refs.imageGallery.insertAdjacentHTML('beforeend', markup);
+function btnHidden() {
+  refs.moreBtn.classList.add('is-hidden');
+}
+
+function btnVisible() {
+  refs.moreBtn.classList.remove('is-hidden');
 }
 
 function clearGallery() {
   refs.imageGallery.innerHTML = '';
+}
+function appendHitsMarkup(hits) {
+  const markup = hits.map(({ largeImageURL, webformatURL, tags, likes, views, comments, downloads }) => {
+    return `<div class="photo-card">
+  <a href='${largeImageURL}'>
+    <img src='${webformatURL}' alt='${tags}' loading="lazy" width="480" />
+  </a>
+  
+  <div class="info">
+    <p class="info-item">
+      <b>Likes: ${likes}</b>
+    </p>
+    <p class="info-item">
+      <b>Views: ${views}</b>
+    </p>
+    <p class="info-item">
+      <b>Comments: ${comments}</b>
+    </p>
+    <p class="info-item">
+      <b>Downloads: ${downloads}</b>
+    </p>
+  </div>
+</div>`
+  }).join('');
+  refs.imageGallery.insertAdjacentHTML('beforeend', markup);
 }
